2442fe6df16e3c28ebdfb044baf905eb62303395
Marcello Alexandre <marcello.alexandre@weni.ai>
2024-10-12 19:45:48 -0300

    Add initial calc in golang

--- main.go
+++ main.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+func sum(a int, b int) int {
+	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func mult(a int, b int) int {
+	return a * b
+}
+
+func div(a int, b int) int {
+	return a / b
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Calculadora #GoEssentials")
+
+	result := sum(5, 10)
+	fmt.Println(result)
+	result = sub(50, 10)
+	fmt.Println(result)
+	result = div(10, 5)
+	fmt.Println(result)
+	result = mult(5, 100)
+	fmt.Println(result)
+
 }
